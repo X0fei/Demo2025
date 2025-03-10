5f1ac1dd65d02c5be21543cb1d0d0cdb00ae92cf
X0fei <pitonvano05@gmail.com>
2025-03-10 15:08:02 +0300

    Database connected

---
+++ Demo2025/Models/Partner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo2025.Models;
+
+public partial class Partner
+{
+    public int Id { get; set; }
+
+    public int PartnerType { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string Adress { get; set; } = null!;
+
+    public string Tin { get; set; } = null!;
+
+    public int Rating { get; set; }
+
+    public string? Director { get; set; }
+
+    public virtual PartnerType PartnerTypeNavigation { get; set; } = null!;
+
+    public virtual ICollection<PartnersProduct> PartnersProducts { get; set; } = new List<PartnersProduct>();
+}
---
+++ Demo2025/Models/PartnersProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo2025.Models;
+
+public partial class PartnersProduct
+{
+    public int Id { get; set; }
+
+    public int Partner { get; set; }
+
+    public int Product { get; set; }
+
+    public int ProductQuantity { get; set; }
+
+    public DateOnly SaleDate { get; set; }
+
+    public virtual Partner PartnerNavigation { get; set; } = null!;
+
+    public virtual Product ProductNavigation { get; set; } = null!;
+}
